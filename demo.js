1f46019fd9636b3f589ea11cdf52a5afc49e8491
ajay7398 <111785506+ajay7398@users.noreply.github.com>
2022-09-08 12:09:51 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,12 @@
+console.log("welcome to parul");
+var a=12;
+var b=34;
+var c=a+b;
+//template lateral
+console.log(`the sum of numbers is:${c}`);
+if(c==46){
+    console.log("the nmber is positive.");
+}
+else{
+    console.log("the numer is negative.");
+}
